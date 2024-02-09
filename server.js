1ac4d68db707a9f07de8b79162e440a916d94dde
ASHLOUISE <paluzziashley@gmail.com>
2024-02-09 12:56:45 -0800

    changed htlm files to handlebars

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = require("./controllers"); 
 const path = require("path")
 const exhbs = require("express-handlebars")
-// import sequelize connection
+
 const sequelize = require("./config/connection");
 
 
@@ -10,7 +10,7 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exhbs.create({}); // add helper functions if needed, dont forget to add the top
+const hbs = exhbs.create({}); 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
@@ -19,9 +19,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(routes); 
 
-// sync sequelize models to the database, then turn on the server
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
 });
+
