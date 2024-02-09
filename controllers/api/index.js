a8832bc94ee4c35e7f1da0c4415ca67d012577ce
Kevin Hogan <kchogan@pacbell.net>
2024-02-09 11:44:12 -0800

    home-page route working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,21 +1,11 @@
 const router = require('express').Router();
 const UserController = require('./user-routes');
-const SandwichController = require('./sandwich-routes');
-const RatingController = require('./rating-routes');
+// const SandwichController = require('./sandwich-routes');
+// const RatingController = require('./rating-routes');
 
-// // User Routes
-// router.post('/users/login', UserController.login); 
-// router.post('/users/register', UserController.register); 
-
-// // Sandwich Routes
-// router.post('/sandwiches', SandwichController.create); 
-// router.get('/sandwiches/:id', SandwichController.get); 
-
-// // Rating Routes
-// router.post('/ratings/sandwich/:id', RatingController.rate); 
-router.use('/sandwiches', SandwichController);
+// router.use('/sandwiches', SandwichController);
 router.use('/users', UserController);
-router.use('/ratings', RatingController);
+// router.use('/ratings', RatingController);
 
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,9 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
-const api = require("./api")
-// const sandwichRoutes = require('./api/sandwich-routes');
-// const userRoutes = require('./api/user-routes');
-// const ratingRoutes = require('./api/rating-routes');
-
-
+// const api = require("./api")
 
 // Routes
 router.use('/', homeRoutes);
-// app.use('/api/sandwiches', sandwichRoutes);
-// app.use('/api/users', userRoutes);
-// app.use('/api/ratings', ratingRoutes);
-app.use('/api', api);
+// router.use('/api', api);
+
 module.exports= router
