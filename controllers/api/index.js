7e73a7529b57231eb6206f1002a2b48a530688b5
philsloan <pjs.tgj@gmail.com>
2024-02-09 14:19:11 -0500

    Added utils auth, fixed and cleaned controlloers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const UserController = require('./user-routes');
 const SandwichController = require('./sandwich-routes');
 const RatingController = require('./rating-routes');
 
-// User Routes
-router.post('/users/login', UserController.login); 
-router.post('/users/register', UserController.register); 
+// // User Routes
+// router.post('/users/login', UserController.login); 
+// router.post('/users/register', UserController.register); 
 
-// Sandwich Routes
-router.post('/sandwiches', SandwichController.create); 
-router.get('/sandwiches/:id', SandwichController.get); 
+// // Sandwich Routes
+// router.post('/sandwiches', SandwichController.create); 
+// router.get('/sandwiches/:id', SandwichController.get); 
 
-// Rating Routes
-router.post('/ratings/sandwich/:id', RatingController.rate); 
+// // Rating Routes
+// router.post('/ratings/sandwich/:id', RatingController.rate); 
+router.use('/sandwiches', SandwichController);
+router.use('/users', UserController);
+router.use('/ratings', RatingController);
+
 
 module.exports = router;
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
-const sandwichRoutes = require('./api/sandwich-routes');
-const userRoutes = require('./api/user-routes');
-const ratingRoutes = require('./api/rating-routes');
+const api = require("./api")
+// const sandwichRoutes = require('./api/sandwich-routes');
+// const userRoutes = require('./api/user-routes');
+// const ratingRoutes = require('./api/rating-routes');
 
 
 
 // Routes
 router.use('/', homeRoutes);
-app.use('/api/sandwiches', sandwichRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/ratings', ratingRoutes);
-
+// app.use('/api/sandwiches', sandwichRoutes);
+// app.use('/api/users', userRoutes);
+// app.use('/api/ratings', ratingRoutes);
+app.use('/api', api);
 module.exports= router
