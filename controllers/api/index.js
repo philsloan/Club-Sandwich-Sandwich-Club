eaab372651d9e6de13674bd46657a78615135598
philsloan <pjs.tgj@gmail.com>
2024-02-09 15:54:36 -0500

    Sandwich route fixed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const UserController = require('./user-routes');
-// const SandwichController = require('./sandwich-routes');
+const SandwichController = require('./sandwich-routes');
 // const RatingController = require('./rating-routes');
 
-// router.use('/sandwiches', SandwichController);
+router.use('/sandwiches', SandwichController);
 router.use('/users', UserController);
 // router.use('/ratings', RatingController);
 
