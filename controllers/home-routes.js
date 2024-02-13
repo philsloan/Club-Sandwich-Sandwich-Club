6d3b124c389a79eb7618cd3c36271cd4f760350d
Kevin Hogan <kchogan@pacbell.net>
2024-02-13 15:23:55 -0800

    homepage now shows top 3 from db instead of hardcoded

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,21 +17,15 @@ router.get("/", async (req, res) => {
       ],
       limit: 3,
       attributes: {
-        include: [
-          [
-            sequelize.literal(
-              "(SELECT AVG(rating) FROM ratings WHERE ratings.sandwich_id = sandwich.id)"
-            ),
-            "avgRating",
-          ],
-        ],
+        include: [[sequelize.literal("(SELECT AVG(rating) FROM ratings WHERE ratings.sandwich_id = sandwich.id)"), "avgRating",],],
       },
       order: [[sequelize.literal("avgRating"), "DESC"]],
     });
 
-    const ratingsList = ratingDbList.map((rating) => rating.get({ plain: true }));
+    const ratingsList = ratingDbList.map((rating) =>
+      rating.get({ plain: true })
+    );
     res.render("homepage", { ratingsList });
-    // res.status(200).json(ratingDbList)
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -40,22 +34,10 @@ router.get("/", async (req, res) => {
 
 router.get("/login", async (req, res) => {
   res.render("login");
-}
-);
-
+});
 
 router.get("/signup", async (req, res) => {
   res.render("signup");
-}
-);
-
-
+});
 
 module.exports = router;
-
-
-
-
-
-
-
--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,11 @@
 var buttonsSource = document.getElementById("button-template").innerHTML;
-            var buttonsTemplate = Handlebars.compile(buttonsSource);
+var buttonsTemplate = Handlebars.compile(buttonsSource);
 
-        
-            var buttonsData = [
-                { label: "NOT WHAT YOU'RE LOOKING FOR?", buttonText: "Click here for more" },
-                { label: "FEELING LUCKY?", buttonText: "Let us pick for you" }
-            ];
+var buttonsData = [
+  { label: "NOT WHAT YOU'RE LOOKING FOR?", buttonText: "Click here for more" },
+  { label: "FEELING LUCKY?", buttonText: "Let us pick for you" },
+];
 
-            
-            var buttonsHtml = buttonsTemplate({ buttons: buttonsData });
+var buttonsHtml = buttonsTemplate({ buttons: buttonsData });
 
-            
-            document.querySelector(".link-boxes").innerHTML = buttonsHtml;
-
+document.querySelector(".link-boxes").innerHTML = buttonsHtml;
