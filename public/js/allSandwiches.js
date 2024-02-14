d89010f88c150333d08e81b8acbfea25869cf13c
Kevin Hogan <kchogan@pacbell.net>
2024-02-14 15:09:28 -0800

    refactored star button event listener code

--- public/js/allSandwiches.js
+++ public/js/allSandwiches.js
@@ -1,8 +1,9 @@
-$(document).ready(function () {
-    $('.rating-container .star').click(function () {
-        var value = $(this).data('value');
-        $(this).siblings('.star').removeClass('active').css('color', 'green');
-        $(this).prevAll('.star').addBack().addClass('active').css('color', 'orange');
-        
-    });
-});
+const starEl = $('.rating-container .star');
+
+const handleStarClick = (event) => {
+    var value = $(event.target).data('value');
+    $(event.target).siblings('.star').removeClass('active').css('color', 'green');
+    $(event.target).prevAll('.star').addBack().addClass('active').css('color', 'orange');
+}
+
+starEl.click(handleStarClick);
