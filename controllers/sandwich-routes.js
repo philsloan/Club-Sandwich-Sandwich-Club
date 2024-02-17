b13dc1018f163a5e8a2920ad04f5e0c909572061
Kevin Hogan <kchogan@pacbell.net>
2024-02-17 14:47:22 -0800

    added in login checks

--- controllers/sandwich-routes.js
+++ controllers/sandwich-routes.js
@@ -10,90 +10,108 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/allSandwiches", async (req, res) => {
-  try {
-    const rawSandwichData = await Sandwich.findAll({ order: [["id", "ASC"]] });
-    const sandwichArray = rawSandwichData.map((sandwich) =>
-      sandwich.get({ plain: true })
-    );
-    const userRatingData = await Rating.findAll({
-      where: { user_id: req.session.user_id },
-      order: [["sandwich_id", "ASC"]],
-    });
-    const userSandwich = userRatingData.map((sandwich) =>
-      sandwich.get({ plain: true })
-    );
-    const sandwichRating = sandwichArray.map((sandwich) => {
-      for (let i = 0; i < userSandwich.length; i++) {
-        if (sandwich.id === userSandwich[i].sandwich_id) {
-          sandwich.userRating = userSandwich[i].rating;
-          return sandwich;
+  if (req.session.logged_in) {
+    try {
+      const rawSandwichData = await Sandwich.findAll({
+        order: [["id", "ASC"]],
+      });
+      const sandwichArray = rawSandwichData.map((sandwich) =>
+        sandwich.get({ plain: true })
+      );
+      const userRatingData = await Rating.findAll({
+        where: { user_id: req.session.user_id },
+        order: [["sandwich_id", "ASC"]],
+      });
+      const userSandwich = userRatingData.map((sandwich) =>
+        sandwich.get({ plain: true })
+      );
+      const sandwichRating = sandwichArray.map((sandwich) => {
+        for (let i = 0; i < userSandwich.length; i++) {
+          if (sandwich.id === userSandwich[i].sandwich_id) {
+            sandwich.userRating = userSandwich[i].rating;
+            return sandwich;
+          }
         }
-      }
-      sandwich.userRating = 0;
-      return sandwich;
-    });
+        sandwich.userRating = 0;
+        return sandwich;
+      });
 
-    res.render("allSandwiches", {
-      sandwichRating,
-      loggedIn: req.session.logged_in,
-      raterId: req.session.user_id,
-    });
-  } catch (error) {
-    console.error("Error fetching sandwiches:", error);
-    res.status(500).send("Internal Server Error");
+      res.render("allSandwiches", {
+        sandwichRating,
+        loggedIn: req.session.logged_in,
+        raterId: req.session.user_id,
+      });
+    } catch (error) {
+      console.error("Error fetching sandwiches:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  } else {
+    res.render("./login");
   }
 });
 
 router.get("/search", async (req, res) => {
-  try {
-    const searchTerm = req.query.term.toLowerCase();
-    console.log("Search Term:", searchTerm);
-    const sandwich = await Sandwich.findOne({ where: { name: searchTerm } });
-    console.log("Found Sandwich:", sandwich);
-    if (sandwich) {
-      const cleanSandwich = sandwich.get({ plain: true });
-      res.render("sandwich", { sandwich: cleanSandwich });
-    } else {
-      res.render("sandwich", {
-        message: "No sandwich found for the given term.",
-      });
+  if (req.session.logged_in) {
+    try {
+      const searchTerm = req.query.term.toLowerCase();
+      console.log("Search Term:", searchTerm);
+      const sandwich = await Sandwich.findOne({ where: { name: searchTerm } });
+      console.log("Found Sandwich:", sandwich);
+      if (sandwich) {
+        const cleanSandwich = sandwich.get({ plain: true });
+        res.render("sandwich", { sandwich: cleanSandwich });
+      } else {
+        res.render("sandwich", {
+          message: "No sandwich found for the given term.",
+        });
+      }
+    } catch (error) {
+      console.error("Error searching for sandwich:", error);
+      res.status(500).send("Internal Server Error");
     }
-  } catch (error) {
-    console.error("Error searching for sandwich:", error);
-    res.status(500).send("Internal Server Error");
+  } else {
+    res.render("./login");
   }
 });
 
 router.get("/newSandwich", async (req, res) => {
-  res.render("newSandwich", {
-    fields,
-    userId: req.session.user_id,
-    loggedIn: req.session.logged_in,
-  });
+  if (req.session.logged_in) {
+    res.render("newSandwich", {
+      fields,
+      userId: req.session.user_id,
+      loggedIn: req.session.logged_in,
+    });
+  } else {
+    res.render("./login");
+  }
 });
 
 router.get("/:id", async (req, res) => {
-  try {
-    const rawSandwichData = await Sandwich.findByPk(req.params.id, {
-      include: [{ model: Rating }, { model: User }],
-    });
-    const sandwichData = rawSandwichData.get({ plain: true });
-    sandwichData.userRating = 0;
-    for (let i = 0; i < sandwichData.ratings.length; i++) {
-      if (sandwichData.ratings[i].user_id == req.session.user_id) {
-        sandwichData.userRating = sandwichData.ratings[i].rating;
-        break;
+  if (req.session.logged_in) {
+    try {
+      const rawSandwichData = await Sandwich.findByPk(req.params.id, {
+        include: [{ model: Rating }, { model: User }],
+      });
+      const sandwichData = rawSandwichData.get({ plain: true });
+      sandwichData.userRating = 0;
+      for (let i = 0; i < sandwichData.ratings.length; i++) {
+        if (sandwichData.ratings[i].user_id == req.session.user_id) {
+          sandwichData.userRating = sandwichData.ratings[i].rating;
+          break;
+        }
       }
+      // res.status(200).json(sandwichData);
+      res.render("sandwich", {
+        sandwich: sandwichData,
+        loggedIn: req.session.logged_in,
+        raterId: req.session.user_id,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
     }
-    // res.status(200).json(sandwichData);
-    res.render("sandwich", {
-      sandwich: sandwichData,
-      loggedIn: req.session.logged_in,
-      raterId: req.session.user_id,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+  } else {
+    res.render("./login");
   }
 });
 
