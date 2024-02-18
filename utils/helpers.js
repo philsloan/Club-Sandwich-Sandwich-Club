61cfaef88cbea26eb48f5ffafc46ead7c36d70b7
Kevin Hogan <kchogan@pacbell.net>
2024-02-17 23:45:14 -0800

    rounding average rating

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,4 +5,7 @@ module.exports = {
   lt: (int1,int2) => int1 < int2,
   eq: (int1,int2) => int1 == int2,
   deq: (int1,int2) => int1 !== int2,
+  format_rating: (rating) => {
+    return Math.round(rating * 100)/100
+  },
 }
